472936ee83e7d17cf9b8098fdd50a13b40db71b2
Dewan Wakil Ahmed <dewanwakilahmed@gmail.com>
2021-10-05 19:53:34 +0600

    Report Page
    Done

--- src/pages/report/report.component.jsx
+++ src/pages/report/report.component.jsx
@@ -4,14 +4,37 @@ import "./report.style.scss";
 
 // COMPONENTS
 import SearchBox from '../../components/search-box/search-box.component';
+import Calendar from '../../components/calendar/calendar.component';
 
 const Report = () => (
     <div className="report">
         <div className="report--main">
-            <SearchBox />
+            <div className="report--main__header">
+                <p className="live">Live</p>
+                <SearchBox />
+            </div>
+            <div className="report--main__body">
+                <div className="live-reports">
+                    <div className="report-block first-block">
+                        &nbsp;
+                    </div>
+                    <div className="report-block">
+                        &nbsp;
+                    </div>
+                    <div className="report-block">
+                        &nbsp;
+                    </div>
+                    <div className="report-block">
+                        &nbsp;
+                    </div>
+                    <div className="report-block">
+                        &nbsp;
+                    </div>
+                </div>
+            </div>
         </div>
         <div className="report--sidebar">
-            sidebar
+            <Calendar />
         </div>
     </div>
 )
