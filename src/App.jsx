bb47b7d423ceb3bcd2ce5ff00724dab787045450
Dewan Wakil Ahmed <dewanwakilahmed@gmail.com>
2021-10-05 16:51:18 +0600

    Connect Clive Jackson Page
    Done

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ import Events from './pages/events/events.component';
 import EventsSushiMasterClass from './pages/events-sushi-masterclass/events-sushi-masterclass.component';
 import SupplierRestaurent from './pages/supplier-restaurent/supplier-restaurent.component'
 import Connect from './pages/connect/connect.component';
+import ConnectCliveJackson from './pages/connect-clive-jackson/connect-clive-jackson.component';
 import PerksOrBenefits from './pages/perks-or-benefits/perks-or-benefits.component';
 import GeoLocationMap from './pages/geo-location-map/geo-location-map.component';
 import GeoLocationMapRegion from './pages/geo-location-map-region/geo-location-map-region.component';
@@ -45,6 +46,7 @@ function App() {
             <Route path='/events' component={Events} />
             <Route path='/events-sushi-masterclass' component={EventsSushiMasterClass} />
             <Route path='/connect' component={Connect} />
+            <Route path='/connect-clive-jackson' component={ConnectCliveJackson} />
             <Route path='/perks-or-benefits' component={PerksOrBenefits} />
             <Route path='/restaurant-module' component={RestaurantModule}/>
             <Route path='/geo-location-map' component={GeoLocationMap}/>
---
+++ src/components/connect-clive-jackson-sidebar/connect-clive-jackson-sidebar.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+
+// ICON
+import arrow from '../../assets/icon/icon-arrow-left.svg'
+
+import './connect-clive-jackson-sidebar.style.scss'
+
+const ConnectCliveJacksonSidebar = () => (
+    <div className="connect-clive-jackson-sidebar">
+        <div className="connect-clive-jackson-sidebar__header">
+            <img src={arrow} alt="arrow icon" />
+            <p>All Connections</p>
+        </div>
+        <ul className="side-nav">
+            <li className="side-nav__item">
+                <a href="#" className="side-nav__link"> Sustainability Take Off’ Clive Jackson</a>
+            </li>
+            <li className="side-nav__item">
+                <a href="#" className="side-nav__link">Connect</a>
+            </li>
+            <li className="side-nav__item">
+                <a href="#" className="side-nav__link">Connect</a>
+            </li>
+            <li className="side-nav__item">
+                <a href="#" className="side-nav__link">Connect</a>
+            </li>
+            <li className="side-nav__item">
+                <a href="#" className="side-nav__link">Connect</a>
+            </li>
+            <li className="side-nav__item">
+                <a href="#" className="side-nav__link">Connect</a>
+            </li>
+            <li className="side-nav__item">
+                <a href="#" className="side-nav__link">Connect</a>
+            </li>
+            <li className="side-nav__item">
+                <a href="#" className="side-nav__link">Connect</a>
+            </li>
+            <li className="side-nav__item">
+                <a href="#" className="side-nav__link">Connect</a>
+            </li>
+            <li className="side-nav__item">
+                <a href="#" className="side-nav__link">Connect</a>
+            </li>
+            <li className="side-nav__item">
+                <a href="#" className="side-nav__link">Connect</a>
+            </li>
+            <li className="side-nav__item">
+                <a href="#" className="side-nav__link">Connect</a>
+            </li>
+            <li className="side-nav__item">
+                <a href="#" className="side-nav__link">Connect</a>
+            </li> 
+            <li className="side-nav__item">
+                <a href="#" className="side-nav__link">Connect</a>
+            </li>
+            <li className="side-nav__item">
+                <a href="#" className="side-nav__link">Connect</a>
+            </li>             
+        </ul>
+    </div>
+)
+
+export default ConnectCliveJacksonSidebar;
