6b95dbeefccfafa4e2d46f9ce66b3214ccd622ea
Khandaker Nabiul <na.ninad60@gmail.com>
2021-10-15 03:50:22 +0600

    geo location map page
    responsive done

--- src/pages/geo-location-map/geo-location-map.component.jsx
+++ src/pages/geo-location-map/geo-location-map.component.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import GoogleMapComponent from '../../components/google-map/google-map.component';
+import GoogleMapComponent from '../../components/google-map/google-map.component'; 
 
 import "./geo-location-map.style.scss";
 
