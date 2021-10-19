b748a90ceae7623c68274ae78d8fefbdc47bee85
Khandaker Nabiul <na.ninad60@gmail.com>
2021-10-19 19:38:51 +0600

    header component
    notification and message dropdown complete

--- src/components/chat-box/chat-box.component.jsx
+++ src/components/chat-box/chat-box.component.jsx
@@ -5,14 +5,14 @@ import image from '../../assets/image/Alex.jpg'
 
 import './chat-box.style.scss'
 
-const ChatBox = () => (
-    <div className="chat-box">
+const ChatBox = ({messageNotification}) => (
+    <div className={messageNotification ? "modify-chat-box chat-box" : "chat-box"}>
         <img src={image} alt="person image" className='image'/>
         <div className='chat-box__info'>
             <p className='title'>Wimbledon Men’s Final 2021</p>
             <p className='last-text'>Let me get on that for you! I’ll be in touch soon.</p>
             <span className='time'>Just Now</span>
-            <span className='counter'>2</span>
+            <span className={messageNotification ? 'modify-counter' : 'counter'}>2</span>
         </div>
     </div>
 )
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,7 +2,7 @@ import React, {useState} from 'react'
 import { Link } from 'react-router-dom'
 
 // COMPONENT
-import ProfileDropdown from '../profile-dropdown/profile-dropdown.component'
+import ChatBox from '../../components/chat-box/chat-box.component'
 
 // ICON
 import search from '../../assets/icon/icon-search.svg'
@@ -12,6 +12,7 @@ import menu from '../../assets/icon/icon-feather-menu.svg'
 import { FaUserAlt } from "react-icons/fa";
 import { FaCog } from "react-icons/fa";
 import { FaSignOutAlt } from "react-icons/fa";
+import { FaUserLock } from "react-icons/fa";
 
 //LOGO
 import logo from '../../assets/logo/wimb-logo.svg'
@@ -25,11 +26,23 @@ import '../../sass/typography.scss'
 const Header = ({title, sidebarStatus}) => {
     
     const [profileDropdown, setProfileDropdown] = useState(true)
+    const [notifDropdown, setNotifDropdown] = useState(true)
+    const [messageDropdown, setMessageDropdown] = useState(true)
 
-    const handleProfileDropdown = () => {
-        setProfileDropdown(!profileDropdown)
+    const handleDropdown = (props) => {
+        switch (props) {
+            case 'profile':
+                setProfileDropdown(!profileDropdown)                
+                break;
+            case 'notification':
+                setNotifDropdown(!notifDropdown)
+                break;
+            case 'message':
+                setMessageDropdown(!messageDropdown)
+                break;
+        }
 
-        console.log(profileDropdown)
+        console.log(notifDropdown)
     }
 
     return (
@@ -49,20 +62,41 @@ const Header = ({title, sidebarStatus}) => {
                     </button>
                 </div>
                 <div className="btn-container">
-                    <button className="notifi-btn">
+                    <button className="notifi-btn" onClick={() => handleDropdown('message')}>
                         <img src={mail} alt="mail-icon" className='icon icon-mail' />
-                    <span className='counter'>2</span>
+                        <span className='counter'>2</span>
                     </button>
+                    <div className={ messageDropdown ? "message-dd" : "message-dd visible"}>
+                        <h3 className='heading-3 mb-1'>Messages</h3>
+                        <hr className='horizontal-line'/>
+                        <div className="message-item">
+                            <ChatBox messageNotification/>
+                            <ChatBox messageNotification/>
+                        </div>
+
+                    </div>
                 </div>
                 <div className="btn-container">
-                    <button className="notifi-btn">
+                    <button className="notifi-btn" onClick={() => handleDropdown('notification')}>
                         <img src={notification} alt="notification-icon" className='icon icon-notifi' />
-                    <span className='counter'>2</span>
+                        <span className='counter'>2</span>
                     </button>
+                    <div className={ notifDropdown ? "notification-dd" : "notification-dd visible"}>
+                        <h3 className='heading-3 mb-1'>Notification</h3>
+                        <hr className='horizontal-line'/>
+                        <div className="notification-dd__item">
+                            <span className='notif-icon'><FaUserLock/></span>
+                            <p>Profile privacy update the ashd ashdjas asdhja sd</p>
+                        </div>
+                        <div className="notification-dd__item">
+                            <span className='notif-icon'><FaUserLock/></span>
+                            <p>Profile privacy update the ashd ashdjas asdhja sd</p>
+                        </div>
+                    </div>
                 </div>
             </div>
             <div className="header__status--profile">
-                <button className="header__status--profile-btn" onClick={handleProfileDropdown}>
+                <button className="header__status--profile-btn" onClick={() => handleDropdown('profile')}>
                     <p className='name'>Alexandra Pratt</p>
                     <img src={user} alt="user-image" className='image'/>
                 </button>
